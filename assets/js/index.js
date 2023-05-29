c31ec472e30964a87c71fd9d3936918dae62f5cb
Mehmet Samet Dursun <dursunmehmetsamet@gmail.com>
2023-05-29 12:26:20 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function openhamburger() {
+  document.getElementById("hamburgermenu").style.display = "block";
+}
+
+function quithamburger() {
+  document.getElementById("hamburgermenu").style.display = "none";
+}
